65a4249f871482cbfce93b73cee5c8eb50c0c701
Kevin Chan <kefin@makedostudio.com>
2013-01-12 16:19:52 -0800

    Bumped version to 0.1.3.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = "django-garage",
-    version = "0.1.2",
+    version = "0.1.3",
     packages = ['garage'],
     include_package_data = True,
     license = "BSD",

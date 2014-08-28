e6c2d70f0cddf9c0cd8a34b9ef1d49d54d53b878
Kevin Chan <kefin@makedostudio.com>
2014-08-27 20:12:27 -0700

    Updated version info in garage.__init__ (for compatibility with PEP 440).
    --HG--
    branch : dev

--- garage/__init__.py
+++ garage/__init__.py
@@ -25,42 +25,22 @@ except (ImportError, ImproperlyConfigured):
 
 
 # package version
-VERSION = (0, 1, 8, 'alpha', 0)
+__author__ = 'Kevin Chan'
+__version_info__ = (0, 1, 8)
+__version__ = '.'.join(map(str, __version_info__))
+
+VERSION = __version_info__
 
 def get_version(version=None):
     """
-    Copied from django.utils.version.
-
-    Returns a PEP 386-compliant version number from VERSION.
+    Returns a PEP 440 version string (in the form of major.minor.micro).
+    Http://legacy.python.org/dev/peps/pep-0440/
     """
     if version is None:
-        version = VERSION
-    else:
-        assert len(version) == 5
-        assert version[3] in ('alpha', 'beta', 'rc', 'final')
-
-    # Now build the two parts of the version number:
-    # main = X.Y[.Z]
-    # sub = .devN - for pre-alpha releases
-    #     | {a|b|c}N - for alpha, beta and rc releases
-
-    parts = 2 if version[2] == 0 else 3
-    main = '.'.join(str(x) for x in version[:parts])
-
-    sub = ''
-
-    # * Not used for this package.
-    # if version[3] == 'alpha' and version[4] == 0:
-    #     git_changeset = get_git_changeset()
-    #     if git_changeset:
-    #         sub = '.dev%s' % git_changeset
-    # elif version[3] != 'final':
-
-    if version[3] != 'final':
-        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
-        sub = mapping[version[3]] + str(version[4])
-
-    return main + sub
+        version = __version_info__
+    if isinstance(version, (list, tuple)):
+        version = '.'.join(map(str, version))
+    return version
 
 
 # helper functions and shortcuts
